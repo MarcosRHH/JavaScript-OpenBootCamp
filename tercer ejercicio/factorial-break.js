4547a9fa0840573f4480d8976dcaf1bfbbfc0ed7
Marcos Rodrigo Huanca Herrera <marco9250197@gmail.com>
2023-06-21 09:46:37 -0400

    tercer ejercicio

---
+++ tercer ejercicio/factorial-break.js
@@ -0,0 +1,9 @@
+let factorial = i = 1;
+while (i <= 10){
+    factorial = factorial * i;
+    if (i == 10) 
+        break;
+    else
+    i++;
+}
+console.log(factorial);
---
+++ tercer ejercicio/factorial-while.js
@@ -0,0 +1,6 @@
+let factorial = i = 1;
+while (i <= 10){
+    factorial = factorial * i;
+    i++;
+}
+console.log(factorial);
