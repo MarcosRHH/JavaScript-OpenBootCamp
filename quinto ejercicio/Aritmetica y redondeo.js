4371d641500d419501f40ea7bcc859ba3d9b121a
MarcosRHH <86620931+MarcosRHH@users.noreply.github.com>
2023-06-22 19:19:39 -0400

    quinto ejercicio

---
+++ quinto ejercicio/Aritmetica y redondeo.js
@@ -0,0 +1,19 @@
+// mi altura en centimetros
+let miAlturaEntero = 175;
+console.log(miAlturaEntero);
+// mi altura en metros
+let miAlturaDecimal = 1.75;
+console.log(miAlturaDecimal);
+// mi peso en kilogramos
+let miPesoDecimal = 48.54;
+console.log(miPesoDecimal);
+// mi altura redondeado hacia arriba
+let miAlturaRedondeada = miAlturaDecimal.toPrecision(2);
+console.log(miAlturaRedondeada);
+// mi altura redondeado hacia abajo
+miPesoRedondeada = miPesoDecimal.toPrecision(3);
+console.log(miPesoRedondeada);
+// maximo valor que se puede obtener en JavaScript
+let maxValor = Number.MAX_VALUE;
+console.log(maxValor);
+console.log(maxValor+1);
