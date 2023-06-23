54ca103c091553e9eb0bccb5c8636d524ec63a05
Marcos Rodrigo Huanca Herrera <marco9250197@gmail.com>
2023-06-23 10:18:59 -0400

    7 - Conjuntos

---
+++ 7 - Conjuntos/Conjuntos.js
@@ -0,0 +1,15 @@
+// Set con los nombres de mi familia
+let miSet = new Set();
+miSet.add('Conti');
+miSet.add('Rosario');
+miSet.add('Rodrigo');
+miSet.add('Milton');
+miSet.add('Vladimir');
+console.log(miSet);
+// aniadiendo duplicado al Set
+miSet.add('Rodrigo');
+console.log(miSet);
+// aniadiendo Javascript a la familia
+miSet.add('Javascript');
+console.log(miSet);
+
