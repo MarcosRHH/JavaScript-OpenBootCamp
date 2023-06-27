a3153fb925a37d6e83d24a4b4bf15079e8a2d953
MarcosRHH <86620931+MarcosRHH@users.noreply.github.com>
2023-06-27 15:07:06 -0400

    EsLint

---
+++ eslint/index.js
@@ -0,0 +1,43 @@
+// iniciar proyecto con node: npm init -y
+// instalar eslint estension
+// crear archivo de correcion de errores: npm install eslint --save-dev
+// luego: npx eslint --init
+// para corregir errores aniadir en el package.json en scripts : "lint:fix": "eslint --fix ."
+// ejecutar en la terminal: npm run lint:fix
+let str_sng = "Hola soy un texto con comillas simples"
+let str_dbl = "Hola soy un texto con comillas dobles"
+
+console.log(str_sng)
+console.log(str_dbl)
+
+let str_comillas = "El otro día me dijo literalmente \"ve a sacar la basura\""
+let str_comillas_simples = "El otro día me dijo literalmente \"ve a sacar la basura\""
+let str_comillas_dobles = "El otro día me dijo literalmente 've a sacar la basura'"
+let str_comillas_simples_2 = "El otro día me dijo literalmente 've a sacar la basura'"
+
+console.log(str_comillas)
+console.log(str_comillas_simples)
+console.log(str_comillas_dobles)
+
+////// Comillas invertidas (backticks)
+let str_backticks = "Hola esto es un string con backticks"
+
+console.log(str_backticks)
+
+let nombre = "Iñigo"
+let saludo = `Hola, ${nombre} bienvenido`
+
+console.log(saludo)
+
+// Plantillas HTML
+let plantilla = `
+<html>
+    <h1>Página web de ${nombre}</h1>
+    <p>Este es un párrafo</p>
+</html>
+`
+
+console.log(plantilla)
+
+//// Introducción de variables en html
+let libros = ["Empieza por el por qué", "El monje que vendió su Ferrari", "El poder del ahora"]
